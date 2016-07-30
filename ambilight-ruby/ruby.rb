be1527996e8a810397624119116402ed2ac3e0d9
Arne <sujomear@arcor.de>
2016-07-30 22:59:12 +0200

    playing around with ruby; imagemagick screencapture takes ~.7s, lets see what c++ takes

---
+++ ambilight-ruby/ruby.rb
@@ -0,0 +1,24 @@
+#simplest ruby program to read from arduino serial,
+#using the SerialPort gem
+#(http://rubygems.org/gems/serialport)
+ 
+require "serialport"
+ 
+#params for serial port
+port_str = "/dev/ttyUSB0"  #may be different for you
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+ 
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+#just read forever
+while true do
+   while (i = sp.gets.chomp) do       # see note 2
+      puts i
+      #puts i.class #String
+    end
+end
+ 
+sp.close                       #see note 1
