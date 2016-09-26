84f8e3d9277d2d59f1ed22b30c09a8a2eb7febc8
enra64 <sujomear@arcor.de>
2016-09-26 17:26:18 +0200

    added own geometry class

---
+++ ambilight-host/include/geometry.h
@@ -0,0 +1,27 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY
+
+template <typename T>
+struct Dimensions {
+	Dimensions(T w, T h, T xOff = 0, T yOff = 0) : width(w), height(h), xOffset(xOff), yOffset(yOff) {
+	}
+
+	Dimensions(){}
+
+	T width = 0;///< object width
+	T height = 0;///< object height
+	T xOffset = 0;///< object horizontal offset
+	T yOffset = 0;///< object vertical offset
+
+	T right() const {
+		return xOffset + width;
+	}
+
+	T bottom() const {
+		return yOffset + height;
+	}
+};
+
+typedef Dimensions<size_t> Geometry;
+
+#endif // !GEOMETRY_H
