b88f612ed1782f58c4089015989801c4e0f98513
Arne <sujomear@arcor.de>
2016-09-28 14:05:12 +0200

    added CImg library to gui plus an convenience include file for everything image library

---
+++ ambilight-gui/include_img_lib.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_IMG_LIB_H
+#define INCLUDE_IMG_LIB_H
+
+// display support is disabled in CImg under unix to avoid including X11 here, which would break building with Qt. (line 266)
+#include "cimg/CImg.h"
+#include "geometry.h"
+typedef cimg_library::CImg<uint8_t> Image;
+
+#endif // INCLUDE_IMG_LIB_H
