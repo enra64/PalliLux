7ccc1cbaef0b9bafcee612dafda013d63b251080
enra64 <sujomear@arcor.de>
2016-12-10 11:01:25 +0100

    refactoring, some ui limits

--- pallilux-gui/spectrometerconfigpage.cpp
+++ pallilux-gui/spectrometerconfigpage.cpp
@@ -14,8 +14,11 @@ SpectrometerConfigPage::SpectrometerConfigPage(QWidget *parent) : QWidget(parent
     connect(ui->offsetSpin, SIGNAL(valueChanged(int)), this, SLOT(parametersUpdated()));
     connect(ui->gainSpin, SIGNAL(valueChanged(double)), this, SLOT(parametersUpdated()));
 
-    ui->lengthSpin->setValue(10);
-    ui->offsetSpin->setValue(100);
+    // limit the offset to acceptable values
+    ui->offsetSpin->setMaximum(mLedConfiguration.getLedCount() - 2);
+
+    ui->lengthSpin->setValue(18);
+    ui->offsetSpin->setValue(90);
     ui->gainSpin->setValue(10);
 }
 
@@ -26,7 +29,20 @@ SpectrometerConfigPage::~SpectrometerConfigPage()
 
 void SpectrometerConfigPage::parametersUpdated() {
     SpectrometerControlWidget* controlWidget = dynamic_cast<SpectrometerControlWidget*>(mCurrentControlWidget);
-    controlWidget->setParameters(ui->offsetSpin->value(), ui->lengthSpin->value(), ui->gainSpin->value());
+    int offset = ui->offsetSpin->value();
+    int ledPerChannel = ui->lengthSpin->value();
+    const int leds = mLedConfiguration.getLedCount();
+
+    // change the maximum led count appropriately to deny more leds than available
+    ui->lengthSpin->setMaximum((leds - offset) / 2);
+
+    if(offset + ledPerChannel * 2 > mLedConfiguration.getLedCount()){
+        // fix the value of the led spinner back down if we exceeded maximum
+        int ledPerChannel = (leds - offset) / 2;
+        ui->lengthSpin->setValue(ledPerChannel);
+    }
+
+    controlWidget->setParameters(offset, ledPerChannel, ui->gainSpin->value());
 }
 
 ControlWidget *SpectrometerConfigPage::getControlWidget(QWidget *) {
--- pallilux/include/colordataproviders/spectrometer/spectrometerhelper.h
+++ pallilux/include/colordataproviders/spectrometer/spectrometerhelper.h
@@ -18,39 +18,70 @@ public:
 
     ~SpectrometerHelper();
 private:
-    LedConfig mLedConfig;
-    const int mFramesPerSecond;///< FPS of the spectrogram.
+    float mGain = 1.f; ///< gain factor. applied before log10(), so the effect is limited
+
+    // variables const for an instance
+    const int FRAMES_PER_SECOND;///< FPS of the spectrogram.
     const double UPPER_FREQUENCY = 3520;///< highest displayed frequency
-    pa_sample_spec mSampleSpecifications; ///< pulseaudio sampling specifcations
-    int mSize;
-    float mGain = 1.f;
+    const int OVERALL_LED_COUNT;///< how many leds does the user have
+    const int SIZE;///< mystery variable
 
-    // pulse audio
-    pa_simple* mPulseAudioDevice;
 
-    uint8_t* mDataBuffer;
+    uint8_t* mLedBuffer;///< internal buffer for saving the led data
 
-    volatile bool mKeepRunning = true;
+    volatile bool mKeepRunning = true;///< true if the working loop should continue
 
-    void calculateAmplitude(fftw_complex* fft, int fftSize, uint8_t *bars, int numLeds);
+    /**
+     * @brief calculate amplitude values given a fft result for a single channel
+     *
+     * for more explanation see https://groups.google.com/d/msg/comp.dsp/cZsS1ftN5oI/rEjHXKTxgv8J
+     * also, https://gitlab.com/nitroxis/pasa/blob/master/pasa.c was most helpful
+     *
+     * @param fft the fft result to work with
+     * @param bars our result buffer. length is determined through mLedsPerChannel
+     */
+    void calculateAmplitude(fftw_complex* fft, uint8_t *bars);
 
-    int mLedOffset;
-    int mLedsPerChannel;
-    int mSelectedLedCount;
-    int OVERALL_LED_COUNT;
+    int mLedOffset;///< how far off the start the spectrum should begin
+    int mLedsPerChannel;///< how many leds are in each of the 2 channels
+    int mSelectedLedCount;///< how many leds are in the spectrum
 
-    // ColorDataProvider interface
-public:
-    void setParameters(int ledOffset, int numberOfLedsPerStereoChannel, double gain){
-        memset(mDataBuffer, 0, OVERALL_LED_COUNT * 3);
-        mLedOffset = (OVERALL_LED_COUNT - 2 * numberOfLedsPerStereoChannel) / 2;
-        mSelectedLedCount = 2 * numberOfLedsPerStereoChannel;
-        mLedsPerChannel = numberOfLedsPerStereoChannel;
-        mLedOffset = ledOffset;
-        mGain = gain;
+
+    /**
+     * @brief Apply the calculated amplitudes to the led buffer
+     * @param barsL the amplitudes of the left channel
+     * @param barsR the amplitudes of the right channel
+     */
+    void applyAmplitudes(uint8_t *barsL, uint8_t *barsR);
+
+    /**
+     * @brief Calculate the hanning window.
+     * @param n dunno
+     * @param N dunno
+     * @return dunno
+     */
+    float windowFunction(int n, int N) {
+        return 0.5f * (1.0f - cosf(2.0f * M_PI * n / (N - 1.0f)));
     }
+public:
+    /**
+     * @brief Change the parameters of this algorithm
+     * @param ledOffset how far off the start of your led strip the spectrogram should start
+     * @param numberOfLedsPerStereoChannel how many leds each channel should have
+     * @param gain a gain factor. applied before a logarithm, so the effect is limited
+     */
+    void setParameters(int ledOffset, int numberOfLedsPerStereoChannel, double gain);
 
+    /**
+     * @brief Get the led data from an internal buffer
+     * @param data the target array
+     * @return the time it took to copy the array
+     */
     float getData(uint8_t *data);
+
+    /**
+     * @brief Start the spectrometer. This function is blocking, you probably want to call it in a thread.
+     */
     void start();
 };
 
